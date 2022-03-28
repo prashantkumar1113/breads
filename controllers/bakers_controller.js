e89af2f5c8a8acd699ceca4a45036263091d2d43
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-03-28 00:30:54 -0500

    Added the virtuals to the Bakers model

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -8,5 +8,14 @@ baker.get("/data/seed", (req, res) => {
     Baker.insertMany(bakerSeedData).then(res.redirect("/breads"));
 });
 
+// Index:
+baker.get("/", (req, res) => {
+    Baker.find()
+        .populate("breads")
+        .then((foundBakers) => {
+            res.json(foundBakers);
+        });
+});
+
 // export
 module.exports = baker;
--- models/baker.js
+++ models/baker.js
@@ -1,16 +1,27 @@
 // dependencies
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
+const Bread = require("./bread");
 
 // schema
-const bakerSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        enum: ["Rachel", "Monica", "Joey", "Chandler", "Ross", "Phoebe"],
+const bakerSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            enum: ["Rachel", "Monica", "Joey", "Chandler", "Ross", "Phoebe"],
+        },
+        startDate: {type: Date, required: true},
+        bio: String,
     },
-    startDate: {type: Date, required: true},
-    bio: String,
+    {toJSON: {virtuals: true}}
+);
+
+// virtuals
+bakerSchema.virtual("breads", {
+    ref: "Bread",
+    localField: "_id",
+    foreignField: "baker",
 });
 
 // model
